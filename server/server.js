090fca0865ba449a886ef70f4bb47f33bb8be050
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-04-29 13:37:42 +0530

    Add server setup and dependencies
    - Updated package.json to include necessary dependencies for the server.
    - Created server.js to initialize the application and connect to the database using mongoose.

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './.env'});
+const app = require('./app');
+
+//connecting to the database
+const db = process.env.DB_URL
+mongoose.connect(db).then(() => {
+    console.log('DB connection successful!');
+}).catch(err => {
+    console.log('DB connection failed!');
+    console.log(err);
+});
+
+const port = process.env.PORT || 3000;
+
